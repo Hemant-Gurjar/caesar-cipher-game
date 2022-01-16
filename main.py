455d634d95250fdd6cb235ecac3d2e187dfb3eee
Hemant Choudhary <globalgrowth07@gmail.com>
2022-01-16 15:09:15 +0000

    fixed a bug for decoding

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ shift = int(input("Type the shift number:\n"))
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def ceaser(start_text, shift_amount, cipher_direction):
   end_text = ""
+  if cipher_direction == "decode":
+       shift_amount *= -1
   for letter in start_text:
     position = alphabet.index(letter)
-    if cipher_direction == "decode":
-       shift_amount *= -1
     new_position = position + shift_amount
     end_text += alphabet[new_position]
   print(f"The {cipher_direction}d text is {end_text}")
