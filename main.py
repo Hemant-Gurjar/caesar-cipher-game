897319d8bc0f4c6262c83b218b36e3f6b1387ffb
Hemant Choudhary <globalgrowth07@gmail.com>
2022-01-16 15:06:16 +0000

    chopped the code

--- main.py
+++ main.py
@@ -5,25 +5,14 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
+def ceaser(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  for letter in start_text:
     position = alphabet.index(letter)
+    if cipher_direction == "decode":
+       shift_amount *= -1
     new_position = position + shift_amount
-    new_letter = alphabet[new_position]
-    cipher_text += new_letter
-  print(f"The encoded text is {cipher_text}")
+    end_text += alphabet[new_position]
+  print(f"The {cipher_direction}d text is {end_text}")
 
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
-  
-if direction == "encode":
-   encrypt(plain_text = text, shift_amount = shift)
-elif direction == "decode":
-   decrypt(cipher_text = text, shift_amount = shift)
+ceaser(start_text = text, shift_amount = shift, cipher_direction = direction)
